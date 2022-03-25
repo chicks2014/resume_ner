372fa4b85abd52e8a098da39f3bd275fe503f39d
saurav <sauravds39@gmail.com>
2022-03-25 07:53:19 +0530

    text preprocessing

--- text_preprocessing/text_preprocess.py
+++ text_preprocessing/text_preprocess.py
@@ -1,3 +1,79 @@
 import nltk 
-#testing
-#new test
+from bs4 import BeautifulSoup
+import unicodedata
+import re
+import string
+import spacy
+from nltk.tokenize import ToktokTokenizer
+
+
+nlp = spacy.load('en_core_web_sm')
+
+# function to remove HTML tags
+def remove_html_tags(text):
+    return BeautifulSoup(text, 'html.parser').get_text()
+
+
+# function to remove accented characters
+def remove_accented_chars(text):
+    new_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return new_text    
+
+# function to remove special characters
+def remove_special_characters(text):
+    # define the pattern to keep
+    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]' 
+    return re.sub(pat, '', text) 
+
+# function to remove numbers
+def remove_numbers(text):
+    # define the pattern to keep
+    pattern = r'[^a-zA-z.,!?/:;\"\'\s]' 
+    return re.sub(pattern, '', text)
+
+
+# function to remove punctuation
+def remove_punctuation(text):
+    text = ''.join([c for c in text if c not in string.punctuation])
+    return text  
+
+# function for stemming
+def get_stem(text):
+    stemmer = nltk.porter.PorterStemmer()
+    text = ' '.join([stemmer.stem(word) for word in text.split()])
+    return text
+
+# function to remove special characters
+def get_lem(text):
+    text = nlp(text)
+    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    return text       
+
+
+tokenizer = ToktokTokenizer()
+stopword_list = nltk.corpus.stopwords.words('english')
+# custom: removing words from list
+stopword_list.remove('not')
+# function to remove stopwords
+def remove_stopwords(text):
+    # convert sentence into token of words
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    # check in lowercase 
+    t = [token for token in tokens if token.lower() not in stopword_list]
+    text = ' '.join(t)    
+    return text    
+
+
+ 
+
+
+# function to remove special characters
+def remove_extra_whitespace_tabs(text):
+    #pattern = r'^\s+$|\s+$'
+    pattern = r'^\s*|\s\s*'
+    return re.sub(pattern, ' ', text).strip()  
+
+# function to remove special characters
+def to_lowercase(text):
+    return text.lower()                   
